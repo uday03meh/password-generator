0cb0bd7e33833896a1c5b73dbbdfb84e48b051fe
Uday Mehtani <udaymehtani@gmail.com>
2022-10-16 14:59:07 +0530

    improved design

--- index.js
+++ index.js
@@ -69,5 +69,6 @@ function myFunction() {
     navigator.clipboard.writeText(copyText.value);
   
     // Alert the copied text
-    alert("Copied the text: " + copyText.value);
+    // alert("Copied the text: " + copyText.value);
+    alert("Copied to Clipboard!")
   }
