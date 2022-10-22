666f9a4d5fbd75371c2d45d767027653c6c3d924
SRIHARI S <sshsrn@gmail.com>
2022-10-22 22:57:54 +0530

    minor update
    Signed-off-by: SRIHARI S <sshsrn@gmail.com>

--- index.js
+++ index.js
@@ -67,8 +67,8 @@ function generatePassword() {
             document.getElementById("myInput").value = generatedPassword
         } 
         catch (TypeError) {
-            alert("No Boxes Checked. Kindly check at least one box");
             document.getElementById("myInput").value = "";
+            alert("No Boxes Checked. Kindly check at least one box");
         }
         generatedPassword = ''
         passwordOptions = []
